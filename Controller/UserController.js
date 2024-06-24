bb009770e9fcf4ed6075044c5b88b291200c82ef
Anjula handapangoda <handapangodageachinthana@gmail.com>
2024-06-24 21:34:57 +0530

    add login

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,17 +1,54 @@
 const UserSchema=require('../Model/UserSchema');
+const bcrypt = require('bcrypt');
+const {response} = require("express");
+const saltRounds = 10;
 
+const jwt = require('jsonwebtoken');
 const registerUser=(req,res)=>{
-    const User=new UserSchema({
-        email:req.body.email,
-        password:req.body.password,
-        fullName:req.body.fullName,
-        avatar:req.body.avatar
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const User=new UserSchema({
+            firstName:req.body.firstName,
+            lastName:req.body.lastName,
+            email:req.body.email,
+            password:hash
+        });
+        User.save().then(savedResponse=>{
+            res.status(201).json(savedResponse);
+        }).catch(error=>{
+            res.status(500).json(error);
+        });
     });
-    User.save().then(savedResponse=>{
-        res.status(201).json(savedResponse);
+
+}
+
+const loginUser=(req,res)=>{
+    UserSchema.findOne({email:req.body.email}).then(resultData=>{
+
+        if (resultData){
+            const token = jwt.sign({
+                email:resultData.email,
+                password:resultData.password,
+                firstName:resultData.firstName,
+                lastName:resultData.lastName
+            }, process.env.PRIVATE_KEY);
+            bcrypt.compare(req.body.password, resultData.password, function(err, result) {
+                // result == false
+                if (err){
+                    res.status(401).json({message:'UnAuthorized'})
+                }
+                if (result){
+                    res.status(200).json({message:'Logged', token: token})
+                }else {
+                    res.status(401).json({message:'UnAuthorized'})
+                }
+            });
+        }else {
+            res.status(404).json({message: 'User not found'})
+        }
     }).catch(error=>{
-        res.status(500).json(error);
-    });
+        res.status(500).json(error)
+    })
 }
 
 const getAllUsers = (req, res) => {
@@ -23,7 +60,6 @@ const getAllUsers = (req, res) => {
             res.status(500).json(error);
         });
 };
-const loginUser=(req,res)=>{}
 
 module.exports={
     getAllUsers,
--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,11 +1,14 @@
 const express=require('express');
 const userController=require('../Controller/UserController');
+const {loginUser} = require("../Controller/UserController");
 
 const router=express.Router();
 
 
 
 router.post('/sign-up',userController.registerUser);
+router.post('/login',userController.loginUser);
+
 // Route to get all users
 router.get('/', userController.getAllUsers);
 
